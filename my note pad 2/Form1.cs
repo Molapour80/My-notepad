23f289058f2c20dcd62cc8b7f98fb290fb092c06
Narjes <molapur80@gmail.com>
2024-10-19 22:00:41 +0330

    MYNOTEPAD

---
+++ my note pad 2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_note_pad_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void formatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusbar1.Visible = sToolStripMenuItem.Checked;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void setbackcolor(object sender, EventArgs e)
+        {
+            txtnotepad.BackColor = Color.FromName(((ToolStripMenuItem)sender).Text);
+            foreach (ToolStripMenuItem x in backColorToolStripMenuItem.DropDownItems)
+                if (x.Text==((ToolStripMenuItem )sender ).Text )
+                    x.Checked = true;
+                else
+                    x.Checked = false;
+        }
+
+        private void foreColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.FullOpen = true;
+            colorDialog1.ShowDialog();
+            txtnotepad.ForeColor = colorDialog1.Color;
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.Font = txtnotepad.Font;
+            fontDialog1.ShowDialog();
+            txtnotepad.Font = fontDialog1.Font;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists("c:\\layout.txt"))
+            {
+                string s;
+                s = System.IO.File.ReadAllText("c:\\layout.txt");
+                ToolStripMenuItem temp = new ToolStripMenuItem();
+                temp.Text = s;
+               // txtnotepad.BackColor = Color.FromName(s);
+                setbackcolor(temp, null);
+            }
+        }
+
+        private void wordWrapToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtnotepad.WordWrap = wordWrapToolStripMenuItem.Checked;
+        }
+    }
+}
